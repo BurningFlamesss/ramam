1972c11b1a1d5e145e8bc1df14f1106388e52ae3
AD <burningggflamesss@gmail.com>
2026-05-27 17:51:47 +0545

    add hero section with titles and footer links to Home component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -77,6 +77,36 @@ function Home() {
             </li>
           </ul>
         </div>
+
+        <section className="hero">
+          <header className="hero-header-wrapper">
+            <div className="hero-header hero-header-1">
+              <h1>Ram</h1>
+            </div>
+            <div className="hero-header hero-header-2">
+              <h1>Prasad</h1>
+            </div>
+          </header>
+          <footer className="hero-footer">
+            <div className="hero-footer-symbols">
+              <img src="" alt="" />
+            </div>
+            <div className="hero-footer-scroll-down">
+              <p className="menu">
+                <Link to="/" target="_blank" className="resume-link">Get {"//"} Resume</Link>
+              </p>
+            </div>
+            <div className="hero-footer-tags">
+              <p className="menu">Light Mode: ON</p>
+            </div>
+          </footer>
+        </section>
+
+        <section className="hero-img-holder">
+          <div className="hero-img">
+            <img src="" alt="" />
+          </div>
+        </section>
 			</div>
 		</div>
 	);
