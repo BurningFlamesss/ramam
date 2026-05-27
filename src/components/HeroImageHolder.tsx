5fad78b217f0f3d7d279aeff6ccbea074bf64472
AD <burningggflamesss@gmail.com>
2026-05-27 19:44:59 +0545

    fix: correct logo image path in HeroImageHolder component

--- src/components/HeroImageHolder.tsx
+++ src/components/HeroImageHolder.tsx
@@ -2,7 +2,7 @@ function HeroImageHolder() {
 	return (
 		<section className="hero-img-holder">
 			<div className="hero-img">
-				<img src="/public/logo512.png" alt="" />
+				<img src="/logo512.png" alt="" />
 			</div>
 		</section>
 	);
